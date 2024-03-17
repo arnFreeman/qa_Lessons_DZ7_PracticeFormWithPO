ff3dd96dbc207c73038a8b7f8a1b6ba64f04a3d4
RenatAbdullin <arn_freeman@mail.ru>
2024-03-17 23:23:34 +0500

    add dz13 for Jenkins

--- src/test/java/tests/PracticeFormWithPOForJenkinsTest.java
+++ src/test/java/tests/PracticeFormWithPOForJenkinsTest.java
@@ -18,6 +18,7 @@ public class PracticeFormWithPOForJenkinsTest {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 
     RegistrationPage registrationPage = new RegistrationPage();
