a8d8566f8f96f640ce1ba448a662f785e93548e2
RenatAbdullin <arn_freeman@mail.ru>
2024-03-18 01:01:07 +0500

    add dz13 for Jenkins

--- src/test/java/tests/PracticeFormWithPOForJenkinsTest.java
+++ src/test/java/tests/PracticeFormWithPOForJenkinsTest.java
@@ -2,12 +2,11 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import pages.RegistrationPage;
+
 import static io.qameta.allure.Allure.step;
 
 
@@ -22,6 +21,15 @@ public class PracticeFormWithPOForJenkinsTest {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
+    @AfterEach
+    void addAttacments() {
+        Attach.screenshotAs("Последний скриншот");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+
+
+    }
     RegistrationPage registrationPage = new RegistrationPage();
 
     @Tag("dz13")
